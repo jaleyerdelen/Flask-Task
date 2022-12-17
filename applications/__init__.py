9f3a633c96215e5019b4eea9fc837b8769ccb496
jaleyerdelen <jaleyerdelen@gmail.com>
2022-12-17 13:18:16 +0300

    create base.html

--- applications/__init__.py
+++ applications/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from applications import routes
-
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///applications.db"
 db = SQLAlchemy(app)
 
+from applications import routes
